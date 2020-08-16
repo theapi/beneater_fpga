2b98c78250220f431ba593dc3c97340f8e12bad5
theapi <petermclarke@gmail.com>
2020-08-16 18:18:57 +0100

    parameterised register display

--- computer_8bit/rtl/verilog/vga_display_register.v
+++ computer_8bit/rtl/verilog/vga_display_register.v
@@ -7,13 +7,18 @@
     input [7:0] data_in,     // the value of the register to show
     input [10:0] vga_h,      // the current vertical pixel count being displayed
     input [10:0] vga_v,      // the current horizontal pixel count being displayed
-    input [2:0] bg,          // the colour of the background pixel
     output [2:0] pixel_out,  // The requested  pixel value at vga_h x vga_v
     output display_on        // whether the screen should be displaying this
  );
 
     parameter START_H = 10;  // horizontal pixel where this should appear on the screen
-    parameter START_V = 380; // vertical pixel where this should appear on the screen
+    parameter START_V = 10; // vertical pixel where this should appear on the screen
+    parameter COLOUR_BG = 3'b010;
+    parameter COLOUR_ON = 3'b100;
+    parameter COLOUR_OFF = 3'b000;
+    parameter W = 11'd16; // Width of each "led"
+    parameter H = 11'd16; // Height of each "led"
+    parameter WG = 11'd4; // Gap between each "led"
 
     reg [2:0] out = 0;
     assign pixel_out = out;
@@ -21,23 +26,62 @@
     assign display_on = on;
 
    	always @ (posedge clk) begin
-        if (vga_v >= START_V && vga_v < START_V + 11'd6
-        && vga_h >= START_H - 11'd5 && vga_h < START_H + 11'd80) begin
+        if (vga_v >= START_V && vga_v < START_V + H
+        && vga_h >= START_H && vga_h < START_H + (WG + (W + WG) * 8)) begin
             on <= 1'b1;
-            // 5 x 5 squares that light if the corresponding bit in the data is set.
-            case (vga_h - START_H)
-                 11'd0,  11'd1,  11'd2,  11'd3,  11'd4: out <= data_in[7] ? 3'b100 : 3'b000;
-                11'd10, 11'd11, 11'd12, 11'd13, 11'd14: out <= data_in[6] ? 3'b100 : 3'b000;
-                11'd20, 11'd21, 11'd22, 11'd23, 11'd24: out <= data_in[5] ? 3'b100 : 3'b000;
-                11'd30, 11'd31, 11'd32, 11'd33, 11'd34: out <= data_in[4] ? 3'b100 : 3'b000;
-                11'd40, 11'd41, 11'd42, 11'd43, 11'd44: out <= data_in[3] ? 3'b100 : 3'b000;
-                11'd50, 11'd51, 11'd52, 11'd53, 11'd54: out <= data_in[2] ? 3'b100 : 3'b000;
-                11'd60, 11'd61, 11'd62, 11'd63, 11'd64: out <= data_in[1] ? 3'b100 : 3'b000;
-                11'd70, 11'd71, 11'd72, 11'd73, 11'd74: out <= data_in[0] ? 3'b100 : 3'b000;
-                default: out <= bg;
-            endcase
+            if (vga_h - START_H < WG) begin
+              out <= COLOUR_BG;
+            end
+            else if (vga_h - START_H < WG + W) begin
+              out <= data_in[7] ? COLOUR_ON : COLOUR_OFF;
+            end
+            else if (vga_h - START_H < WG + W + WG) begin
+              out <= COLOUR_BG;
+            end
+            else if (vga_h - START_H < WG + W + WG + W) begin
+              out <= data_in[6] ? COLOUR_ON : COLOUR_OFF;
+            end
+            else if (vga_h - START_H < WG + (W + WG) * 2 ) begin
+              out <= COLOUR_BG;
+            end
+            else if (vga_h - START_H < WG + (W + WG) * 2 + W) begin
+              out <= data_in[5] ? COLOUR_ON : COLOUR_OFF;
+            end
+            else if (vga_h - START_H < WG + (W + WG) * 3) begin
+              out <= COLOUR_BG;
+            end
+            else if (vga_h - START_H < WG + (W + WG) * 3 + W) begin
+              out <= data_in[4] ? COLOUR_ON : COLOUR_OFF;
+            end
+            else if (vga_h - START_H < WG + (W + WG) * 4) begin
+              out <= COLOUR_BG;
+            end
+            else if (vga_h - START_H < WG + (W + WG) * 4 + W) begin
+              out <= data_in[3] ? COLOUR_ON : COLOUR_OFF;
+            end
+            else if (vga_h - START_H < WG + (W + WG) * 5) begin
+              out <= COLOUR_BG;
+            end
+            else if (vga_h - START_H < WG + (W + WG) * 5 + W) begin
+              out <= data_in[2] ? COLOUR_ON : COLOUR_OFF;
+            end
+            else if (vga_h - START_H < WG + (W + WG) * 6) begin
+              out <= COLOUR_BG;
+            end
+            else if (vga_h - START_H < WG + (W + WG) * 6 + W) begin
+              out <= data_in[1] ? COLOUR_ON : COLOUR_OFF;
+            end
+            else if (vga_h - START_H < WG + (W + WG) * 7) begin
+              out <= COLOUR_BG;
+            end
+            else if (vga_h - START_H < WG + (W + WG) * 7 + W) begin
+              out <= data_in[0] ? COLOUR_ON : COLOUR_OFF;
+            end
+            else begin
+              out <= COLOUR_BG;
+            end
         end else begin
-            out <= bg;
+            out <= COLOUR_BG;
             on <= 1'b0;
         end
     end
--- computer_8bit/rtl/verilog/vga_frame.v
+++ computer_8bit/rtl/verilog/vga_frame.v
@@ -29,12 +29,14 @@ module vga_frame(
   // Show the cpu clock value
   wire [2:0] cpu_clock_display_out;
   wire cpu_clock_display_on;
-  vga_display_register #(.START_H(11'd50), .START_V(11'd10)) cpu_clock_display (
+  vga_display_register #(
+    .START_H(11'd50),
+    .START_V(11'd10)
+  ) cpu_clock_display (
     .clk(clk),
     .data_in({8{ cpu_clock }}),
     .vga_h(vga_h),
     .vga_v(vga_v),
-    .bg(3'b010),
     .pixel_out(cpu_clock_display_out),
     .display_on(cpu_clock_display_on)
   );
@@ -42,72 +44,66 @@ module vga_frame(
   // Show the data bus value
   wire [2:0] bus_display_out;
   wire bus_display_on;
-  vga_display_register #(.START_H(11'd50), .START_V(11'd20)) bus_display (
+  vga_display_register #(.START_H(11'd50), .START_V(11'd40)) bus_display (
     .clk(clk),
     .data_in(bus),
     .vga_h(vga_h),
     .vga_v(vga_v),
-    .bg(3'b010),
     .pixel_out(bus_display_out),
     .display_on(bus_display_on)
   );
 
   wire [2:0] switches_display_out;
   wire switches_display_on;
-  vga_display_register #(.START_H(11'd50), .START_V(11'd30)) switches_display (
+  vga_display_register #(.START_H(11'd50), .START_V(11'd70)) switches_display (
     .clk(clk),
     .data_in(switches),
     .vga_h(vga_h),
     .vga_v(vga_v),
-    .bg(3'b010),
     .pixel_out(switches_display_out),
     .display_on(switches_display_on)
   );
 
   wire [2:0] a_reg_display_out;
   wire a_reg_display_on;
-  vga_display_register #(.START_H(11'd50), .START_V(11'd40)) a_reg_display (
+  vga_display_register #(.START_H(11'd50), .START_V(11'd100)) a_reg_display (
     .clk(clk),
     .data_in(a_reg),
     .vga_h(vga_h),
     .vga_v(vga_v),
-    .bg(3'b010),
     .pixel_out(a_reg_display_out),
     .display_on(a_reg_display_on)
   );
 
   wire [2:0] b_reg_display_out;
   wire b_reg_display_on;
-  vga_display_register #(.START_H(11'd50), .START_V(11'd50)) b_reg_display (
+  vga_display_register #(.START_H(11'd50), .START_V(11'd130)) b_reg_display (
     .clk(clk),
     .data_in(b_reg),
     .vga_h(vga_h),
     .vga_v(vga_v),
-    .bg(3'b010),
     .pixel_out(b_reg_display_out),
     .display_on(b_reg_display_on)
   );
 
   wire [2:0] instruction_reg_display_out;
   wire instruction_reg_display_on;
-  vga_display_register #(.START_H(11'd50), .START_V(11'd60)) instruction_reg_display (
+  vga_display_register #(.START_H(11'd50), .START_V(11'd160)) instruction_reg_display (
     .clk(clk),
     .data_in(instruction_reg),
     .vga_h(vga_h),
     .vga_v(vga_v),
-    .bg(3'b010),
     .pixel_out(instruction_reg_display_out),
     .display_on(instruction_reg_display_on)
   );
 
   wire [2:0] sum_display_out;
   wire sum_display_on;
-  vga_display_register #(.START_H(11'd50), .START_V(11'd70)) sum_display (
+  vga_display_register #(.START_H(11'd50), .START_V(11'd190)) sum_display (
     .clk(clk),
     .data_in(sum),
     .vga_h(vga_h),
     .vga_v(vga_v),
-    .bg(3'b010),
     .pixel_out(sum_display_out),
     .display_on(sum_display_on)
   );
