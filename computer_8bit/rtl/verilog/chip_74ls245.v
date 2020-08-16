0d67f9e46320693cfd973720c698e0b04910a428
theapi <petermclarke@gmail.com>
2020-08-16 12:03:39 +0100

    Switches on the bus

--- computer_8bit/rtl/verilog/chip_74ls245.v
+++ computer_8bit/rtl/verilog/chip_74ls245.v
@@ -7,17 +7,21 @@
 module chip_74ls245 (
   input clk,
   input [7:0] in,
+  input [7:0] bus,
   input oe, // Active low
 
   output [7:0] out
 );
 
-  reg [7:0] data_out = 0;
+  reg [7:0] data_out;
   assign out = data_out;
 
   always @(posedge clk) begin
     if (oe == 1'b0) begin
       data_out <= in;
+    end
+    else begin
+      data_out <= bus;
     end
   end
 
